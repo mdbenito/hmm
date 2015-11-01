fb412e551192bac6618a7b6b5306016eded6c05b
Miguel de Benito Delgado <nonick@8027.org>
2015-11-01 01:40:14 +0100

    Handle 1D arrays in is_row_stochastic, don't attempt to use generators

--- utils.py
+++ utils.py
@@ -2,5 +2,9 @@ import numpy as np
 
 
 def is_row_stochastic(M: np.ndarray) -> bool:
-    assert (M.ndim == 2)
-    return np.allclose(M.sum(axis=1), (1. for x in range(0, M.shape[0])))
+    assert(M.ndim == 1 or M.ndim == 2)
+
+    if M.ndim == 1:
+        return np.allclose(M, [1. for x in range(0, M.size)])
+    elif M.ndim == 2:
+        return np.allclose(M.sum(axis=1), [1. for x in range(0, M.shape[0])])
