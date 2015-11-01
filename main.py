5aaba57b0ed44823e2b38960673b150a633afd92
Miguel de Benito Delgado <nonick@8027.org>
2015-11-01 23:26:44 +0100

    Better usage message. Now can run tests from command line

--- main.py
+++ main.py
@@ -11,32 +11,43 @@ import data
 def parse_options(argv):
     input_file = ''
     output_file = ''
-    usage_str = 'hmm.py -i <input_file> -o <output_file>'
+    run_tests = False
+    usage_str = 'Usage: python hmm.py -i <input-file> -o <output-file>\n\nOther options:\n' + \
+                '\t-h, --help\t\tThis help\n' + \
+                '\t-i, --input-file\tSpecifies the input file\n' + \
+                '\t-o, --output-file\tSpecifies the output file\n' + \
+                '\t-t, --run-tests\t\tRun all tests\n'
     try:
-        opts, args = getopt.getopt(argv, "hi:o:", ["input_file=", "output_file="])
+        opts, args = getopt.getopt(argv, 'hti:o:', ['help', 'run-tests', 'input-file=', 'output-file='])
     except getopt.GetoptError:
         print(usage_str)
         sys.exit(2)
     for opt, arg in opts:
-        if opt == '-h':
+        if opt in ('-h', '--help'):
             print(usage_str)
             sys.exit()
-        elif opt in ("-i", "--input_file"):
+        elif opt in ('-i', '--input-file'):
             input_file = arg
-        elif opt in ("-o", "--output_file"):
+        elif opt in ('-o', '--output-file'):
             output_file = arg
-    return [input_file, output_file]
+        elif opt in ('-t', '--run-tests'):
+            run_tests = True
+    return [input_file, output_file, run_tests]
     # print('Input file is "', input_file)
     # print('Output file is "', output_file)
 
 
 def main(argv):
-    [input_file, output_file] = parse_options(argv)
-    # d = data.load_data(input_file)
-    d = data.generate()
-    m = inference.iterate(d, maxiter=5)
+    [input_file, output_file, run_tests] = parse_options(argv)
+    if run_tests:
+        import test_all
+        test_all.main()
+    else:
+        d = data.load(input_file)
+        d = data.generate()
+        m = inference.iterate(d)
+        inference.save(m, output_file)
 
-    # inference.viterbi_path()
 
 if __name__ == '__main__':
     main(sys.argv[1:])
--- test_all.py
+++ test_all.py
@@ -2,9 +2,15 @@ import unittest as ut
 import test_data
 import test_inference
 
-if __name__ == '__main__':
+
+def main():
     suite = ut.TestSuite()
     for case in [test_data.TestMethods,
                  test_inference.TestMethods]:
         suite.addTest(ut.defaultTestLoader.loadTestsFromTestCase(case))
     ut.TextTestRunner(verbosity=2).run(suite)
+
+
+if __name__ == '__main__':
+    main()
+
