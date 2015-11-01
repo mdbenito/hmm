4a34dce57d5b772e51d27bde8d8d7fc88bf926c1
Miguel de Benito Delgado <nonick@8027.org>
2015-11-01 01:43:57 +0100

    is_row_stochastic now works

--- utils.py
+++ utils.py
@@ -5,6 +5,6 @@ def is_row_stochastic(M: np.ndarray) -> bool:
     assert(M.ndim == 1 or M.ndim == 2)
 
     if M.ndim == 1:
-        return np.allclose(M, [1. for x in range(0, M.size)])
+        return np.isclose(M.sum(), 1.)
     elif M.ndim == 2:
         return np.allclose(M.sum(axis=1), [1. for x in range(0, M.shape[0])])
