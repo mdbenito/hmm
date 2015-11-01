95f14beaa90f38b5aac9b251069a24f3a050f063
Miguel de Benito Delgado <nonick@8027.org>
2015-11-01 01:12:33 +0100

    Moved is_row_stochastic to utils

--- inference.py
+++ inference.py
@@ -1,13 +1,7 @@
 from functools import reduce
 import numpy as np
 from data import Data
-
-
-# TODO: remove me or move to test_inference
-def is_row_stochastic(M: np.ndarray) -> bool:
-    assert (M.ndim == 2)
-    return np.allclose(M.sum(axis=1), (1. for x in range(0, M.shape[0])))
-
+from utils import *
 
 class Model:
     """
