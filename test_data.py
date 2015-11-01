394c33449058a343f1797d93ba2d8486118d084f
Miguel de Benito Delgado <nonick@8027.org>
2015-11-01 22:47:34 +0100

    Added test for generated emissions

--- test_data.py
+++ test_data.py
@@ -5,10 +5,13 @@ import data
 
 class TestMethods(ut.TestCase):
     def test_generate(self):
-        d = data.generate()
+        d = data.generate(N=4, M=10, L=1000)
         for M in d.generator.values():
             self.assertTrue(is_row_stochastic(M))
         self.assertEqual(d.Y.size, d.L)
+
+        if not (d.Y >= 0.).all() or not (d.Y <= 9).all():
+            self.fail('Generated emissions are out of range')
 
         # TODO: more of this
 
