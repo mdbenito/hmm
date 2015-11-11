15b13ff1cc03d1805e57bbcf273271e3514baaed
Miguel de Benito Delgado <nonick@8027.org>
2015-11-11 22:38:16 +0100

    Change config

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 # Probaby not much here...
 
-eps= 1e-10  # Precision to use when testing with np.isclose()
-test_eps= 1e-2  # Precision for unit tests
+eps = 1e-10  # Precision to use when testing with np.isclose()
+test_eps = 1e-8  # Precision for unit tests
