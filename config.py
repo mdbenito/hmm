1a2aafbf2ca455f49aa6ac803db3e4f22b70c67c
Miguel de Benito Delgado <nonick@8027.org>
2015-12-30 23:10:17 +0100

    Allow more precision in iteration

--- config.py
+++ config.py
@@ -2,4 +2,4 @@
 
 eps = 1e-10              # Precision to use when testing with np.isclose()
 test_eps = 1e-1          # Precision for unit tests
-iteration_margin = 1e-5  # Minimal percentual increase in log likelihood before stopping
+iteration_margin = 1e-8  # Minimal percentual increase in log likelihood before stopping
