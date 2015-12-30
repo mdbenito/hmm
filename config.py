ab174b88a19c3f0f54e525699df566b71673eaf7
Miguel de Benito Delgado <nonick@8027.org>
2015-12-31 00:32:53 +0100

    Revert optimistic increase in test precision

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # Probably not much here...
 
 eps = 1e-10              # Precision to use when testing with np.isclose()
-test_eps = 1e-2          # Precision for unit tests
-iteration_margin = 1e-8  # Minimal percentual increase in log likelihood before stopping
+test_eps = 1e-1          # Precision for unit tests
+iteration_margin = 1e-8  # Minimal percentage increase in log likelihood before stopping
